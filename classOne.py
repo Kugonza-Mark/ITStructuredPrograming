cf21e27e864822bbf67da48b88de5daf4b0b18d6
Steve09090 <stvogwal@gmail.com>
2024-10-07 17:48:02 +0300

    Celcious spelling
    Nothing serious

--- classOne.py
+++ classOne.py
@@ -1,5 +1,5 @@
 # Question 7
 celciousTemp = float (input("Please enter the temp in Celicious"))
-fareTemp = (ceiliousTemp * 9/5) + 32
-print(f"Your temperature that i got as {ceiliousTemp} and its value in farehenit is {fareTemp}")
+fareTemp = (celciousTemp * 9/5) + 32
+print(f"Your temperature that i got as {celciousTemp} and its value in farehenit is {fareTemp}")
 
