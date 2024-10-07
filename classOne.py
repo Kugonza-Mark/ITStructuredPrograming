dd028ae98f2b53d0c72833feab1b3fbd4253c349
Kugonza-Mark <45218189+Kugonza-Mark@users.noreply.github.com>
2024-10-07 17:43:58 +0300

    answer to number 8

--- classOne.py
+++ classOne.py
@@ -1,5 +1,11 @@
 # Question 7
-ceiliousTemp = float (input("Please enter the temp in Celicious"))
-fareTemp = (ceiliousTemp * 9/5) + 32
-print(f"Your temperature that i got as {ceiliousTemp} and its value in farehenit is {fareTemp}")
+# ceiliousTemp = float (input("Please enter the temp in Celicious"))
+# fareTemp = (ceiliousTemp * 9/5) + 32
+# print(f"Your temperature that i got as {ceiliousTemp} and its value in farehenit is {fareTemp}")
+
 # Question 8
+
+numberOne= int(input("Please enter the first number"))
+numberTwo= int (input("Please enter the second number"))
+answer=numberOne*numberTwo
+print(f"The answer after multiplying {numberOne} by {numberTwo} is {answer}")
