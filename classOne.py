694f404270aac47d39f3bebccede2e12ccf0345e
shalomlilly <shalomlilly91@gmail.com>
2024-10-07 21:16:30 +0300

    code

--- classOne.py
+++ classOne.py
@@ -1,8 +1,8 @@
 # Question 7
 
-celciousTemp = float (input("Please enter the temp in Celicious"))
-fareTemp = (celciousTemp * 9/5) + 32
-print(f"Your temperature that i got as {celciousTemp} and its value in farehenit is {fareTemp}")
+#celciousTemp = float (input("Please enter the temp in Celicious"))
+#fareTemp = (celciousTemp * 9/5) + 32
+#print(f"Your temperature that i got as {celciousTemp} and its value in farehenit is {fareTemp}")
 
 
 # ceiliousTemp = float (input("Please enter the temp in Celicious"))
