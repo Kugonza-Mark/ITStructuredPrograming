5577eba3187a7e470395a5a153dbad5f32beb1f7
Jace-Eric <jaceeric51@gmail.com>
2024-10-07 17:51:46 +0300

    Comment from Eric

--- classOne.py
+++ classOne.py
@@ -5,7 +5,14 @@
 
 # Question 8
 
-numberOne= int(input("Please enter the first number"))
-numberTwo= int (input("Please enter the second number"))
-answer=numberOne*numberTwo
-print(f"The answer after multiplying {numberOne} by {numberTwo} is {answer}")
+# numberOne= int(input("Please enter the first number"))
+# numberTwo= int (input("Please enter the second number"))
+# answer=numberOne*numberTwo
+# print(f"The answer after multiplying {numberOne} by {numberTwo} is {answer}")
+
+#Question 9
+Radius = float(input("Enter the radius of the cylinder here: "))
+Height = float(input("Enter the height of the cylinder here: "))
+VolumeOfTheCylinder = 3.1416*Radius**2*Height
+print(f"The volume of the cylinder is {VolumeOfTheCylinder}")
+
