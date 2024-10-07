933f7665a81d6ed1ae2a8d50cd9b920a97fb81cb
Jace-Eric <jaceeric51@gmail.com>
2024-10-07 17:33:07 +0300

    Comment from Eric

--- classOne.py
+++ classOne.py
@@ -2,3 +2,4 @@
 ceiliousTemp = float (input("Please enter the temp in Celicious"))
 fareTemp = (ceiliousTemp * 9/5) + 32
 print(f"Your temperature that i got as {ceiliousTemp} and its value in farehenit is {fareTemp}")
+# Question 8
