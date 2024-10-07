3cc0a339841d8f67450f4b896640699848624c94
shalomlilly <shalomlilly91@gmail.com>
2024-10-07 21:14:38 +0300

    code

--- classOne.py
+++ classOne.py
@@ -17,3 +17,23 @@ print(f"Your temperature that i got as {celciousTemp} and its value in farehenit
 # print(f"The answer after multiplying {numberOne} by {numberTwo} is {answer}")
 
 #Question 9
+
+#A=int(input("enter the value"))
+#B=int(input("enter the value"))
+#hypotenuse=A**2+B**2
+#print(f"the hypotenuse is {hypotenuse}")
+
+"""M1= 66
+M2= 20
+R= 200
+formula=6.7*10*-11*M1*M2/R
+print(f"the force of gravitational attraction between the two bodies is{formula}")
+"""
+#populationC=43
+#growthRate=123/10
+#inThreeYears=populationC*(1+growthRate)**3
+#print(f"the population in 3 years is{inThreeYears}") 
+
+
+
+
